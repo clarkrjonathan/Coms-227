b539602db2237c5ccce1eab185d3ef8d208a5df9
Jonathan Clark <clarkrjonathan@gmail.com>
2024-12-03 20:33:00 -0600

    classpath change

--- homework4/src/homework4/Main.java
+++ homework4/src/homework4/Main.java
@@ -4,6 +4,14 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello World");
 		System.out.println("Github Test");
+		int[] intarray = {1};
+		test(intarray);
+		System.out.println(intarray[0]);
+	}
+	
+	
+	public static void test(int[] intarray) {
+		intarray[0] += 1;
 	}
 
 }
