83ad1043da3187ca458816534275b648b7a73e6f
Jonathan Clark <clarkrjonathan@gmail.com>
2024-12-11 18:48:07 -0600

    Finalized javadocs fr this time

--- Homework4c/doc/package-search-index.js
+++ Homework4c/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"hw4"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"api"},{"l":"examples"},{"l":"hw4"},{"l":"ui"}];updateSearchResults();
