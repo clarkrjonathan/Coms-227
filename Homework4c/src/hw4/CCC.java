8457c8b44e72da14b5d33f585f8d1abf184f57c4
Jonathan Clark <clarkrjonathan@gmail.com>
2024-12-11 18:17:18 -0600

    more concise rotation method

--- Homework4c/src/hw4/CCC.java
+++ Homework4c/src/hw4/CCC.java
@@ -9,11 +9,6 @@ import api.Position;
  *
  */
 public abstract class CCC extends AbstractPiece{
-	
-	//TODO check declaration
-	private static final Position[] EDGES = {new Position(0,1), new Position(1, 2), new Position(2, 1), new Position(1, 0)};
-	private static final Position[] CORNERS = {new Position(0,0), new Position(0, 2), new Position(2,2), new Position(2, 0)};
-	
 
 	protected CCC(Position position) {
 		super(position);
@@ -31,24 +26,16 @@ public abstract class CCC extends AbstractPiece{
 	}
 	
 	private Position rotatedPos(Position pos) {
-		//TODO make general for all bounding boxes, set coords for center to be origin
+		int row = pos.col();
+		int col = pos.row();
 		
-	
-		Position rotatedPos = new Position(pos.row(),pos.col());
-		for(int i = 0; i < EDGES.length; i++) {
-			if (EDGES[i].compareTo(pos) == 0) {
-				//Immutable object so just copying reference is enough
-				rotatedPos = i < 3 ? EDGES[i + 1] : EDGES[0];
-				
-			} else if (CORNERS[i].compareTo(pos) == 0) {
-				
-				rotatedPos = i < 3 ? CORNERS[i + 1] : CORNERS[0];
-				
-			}
-			
+		if(col == 0) {
+			col = 2;
+		} else if (col == 2) {
+			col = 0;
 		}
 	
-		return rotatedPos;
+		return new Position(row, col);
 	}
 	
 
